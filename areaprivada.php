1a31a9312b4e4340d6d3fbf5ead6ed3031ea7ab0
Leonardo <leonardolandin@bitbucket.org>
2019-06-25 14:58:20 -0300

    completar cadastro

--- areaprivada.php
+++ areaprivada.php
@@ -22,9 +22,12 @@
 			<nav class="nav-user">
 				<ul class="ul-user">
 					<li><a href="conta.php">Minha conta</a></li>
-					<li><a href="perfil_sair.php" >Sair</a></li>
+					<li><a href="perfil_sair.php">Sair</a></li>
 				</ul>
 			</nav>
+			<div class="assine">
+				<input type="button" class="btn-assine" value="Assine Já" name="assine">
+			</div>
 			<div class="input-search">
 				<input type="text" placeholder="Pesquise uma imagem por termo" name="user" class="search">
 				<img src="images/search.png" alt="" class="search-image" />
@@ -37,7 +40,7 @@
 		</label>
 		<nav class="nav-menu">
 			<ul class="ul-menu">
-				<li><a href="atividade.php">Atividades</a></li>
+				<li><a href="areaprivada.php">Atividades</a></li>
 				<li><a href="aula.php">Video-Aulas</a></li>
 				<li><a href="plano.php">Planos</a></li>
 				<li><a href="configurar.php">Configurações</a></li>
@@ -47,15 +50,12 @@
 			<div class="modal-images">
 				<span>Não há imagens para a visualização.</span>
 				<img src="images/sad.png" alt="" class="no-images" />
-<?php
-session_start();
-if(isset($_SESSION['login'])) {
-	
-}
-else {
-    header("location: index.php");
-}
-?>
+				<?php
+				session_start();
+				if (isset($_SESSION['login'])) { } else {
+					header("location: index.php");
+				}
+				?>
 			</div>
 
 		</div>
--- completar_cadastro.php
+++ completar_cadastro.php
@@ -1,194 +1,199 @@
 <html lang="pt-br">
+
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/completar.css">
 </head>
+
 <body>
-	<form method="POST">
-		<div>
-		<p>Como você se considera e relação a informática de um modo geral?</p>
-		<select name="informatica">
-			<?php
-				require_once "usuarios.php";
-				$us = new Usuario;
-				//exibindo os valores da tabela do bd
-				$receber = $us->pegar("informatica");
-				$i=0;
-				while($receber[0][$i]> $i){
-					$valor = $receber[0][$i];
-					$str = $receber[1][$i];
-					echo "<option value='$valor'>$str</option>";
-					$i++;
-				}
-			?>
-		</select>
-		</div>
-		<div>
-			<p>Como você se considera em relação ao uso de editores de texto (word,...)</p>
-			<select name="editores_texto">
-				<?php
-				//exibindo os valores da tabela do bd
+	<div class="fundo-completar">
+		<form method="POST" class="form-completar">
+			<div>
+				<p>Como você se considera e relação a informática de um modo geral?</p>
+				<select name="informatica">
+					<?php
+					require_once "usuarios.php";
+					$us = new Usuario;
+					//exibindo os valores da tabela do bd
+					$receber = $us->pegar("informatica");
+					$i = 0;
+					while ($receber[0][$i] > $i) {
+						$valor = $receber[0][$i];
+						$str = $receber[1][$i];
+						echo "<option value='$valor'>$str</option>";
+						$i++;
+					}
+					?>
+				</select>
+			</div>
+			<div>
+				<p>Como você se considera em relação ao uso de editores de texto (word,...)</p>
+				<select name="editores_texto">
+					<?php
+					//exibindo os valores da tabela do bd
 					$receber = $us->pegar("editor_texto");
-					$i=0;
-					while($receber[0][$i]> $i){
+					$i = 0;
+					while ($receber[0][$i] > $i) {
 						$valor = $receber[0][$i];
 						$str = $receber[1][$i];
 						echo "<option value='$valor'>$str</option>";
 						$i++;
 					}
-				?>
-			</select>
-		</div>
-		<div>
-			<p>Como você se considera em relação a editores de apresentação (Power Point)</p>
-			<select name="apresentacao">
-				<?php
-				//exibindo os valores da tabela do bd
+					?>
+				</select>
+			</div>
+			<div>
+				<p>Como você se considera em relação a editores de apresentação (Power Point)</p>
+				<select name="apresentacao">
+					<?php
+					//exibindo os valores da tabela do bd
 					$receber = $us->pegar("editor_apresentacao");
-					$i=0;
-					while($receber[0][$i]>$i){
+					$i = 0;
+					while ($receber[0][$i] > $i) {
 						$valor = $receber[0][$i];
 						$str = $receber[1][$i];
 						echo "<option value='$valor'>$str</option>";
 						$i++;
 					}
-				?>
-			</select>
-		</div>
-		<div>
-			<p>Ao falarmos de edição de imagens e vídeos. Como você se considera?</p>
-			<select name="imagem_video">
-				<?php
-				//exibindo os valores da tabela do bd
+					?>
+				</select>
+			</div>
+			<div>
+				<p>Ao falarmos de edição de imagens e vídeos. Como você se considera?</p>
+				<select name="imagem_video">
+					<?php
+					//exibindo os valores da tabela do bd
 					$receber = $us->pegar("imagens_videos");
-					$i=0;
-					while($receber[0][$i]>$i){
+					$i = 0;
+					while ($receber[0][$i] > $i) {
 						$valor = $receber[0][$i];
 						$str = $receber[1][$i];
 						echo "<option value='$valor'>$str</option>";
 						$i++;
 					}
-				?>
-			</select>
-		</div>
-		<div>
-			<p>Quando tem alguma dificuldade no uso de recursos multimídia o que costuma fazer?</p>
-			<select name="multimidia">
-				<?php
-				//exibindo os valores da tabela do bd
+					?>
+				</select>
+			</div>
+			<div>
+				<p>Quando tem alguma dificuldade no uso de recursos multimídia o que costuma fazer?</p>
+				<select name="multimidia">
+					<?php
+					//exibindo os valores da tabela do bd
 					$receber = $us->pegar("multimidia");
-					$i=0;
-					while($receber[0][$i]>$i){
+					$i = 0;
+					while ($receber[0][$i] > $i) {
 						$valor = $receber[0][$i];
 						$str = $receber[1][$i];
 						echo "<option value='$valor'>$str</option>";
 						$i++;
 					}
-				?>
-			</select>
-		</div>
-		<div>
-			<p>Quando precisa de atividades acadêmicas e/ou ...., como costuma a consegui-las?</p>
-			<select name="atividades_academicas">
-				<?php
-				//exibindo os valores da tabela do bd
+					?>
+				</select>
+			</div>
+			<div>
+				<p>Quando precisa de atividades acadêmicas e/ou ...., como costuma a consegui-las?</p>
+				<select name="atividades_academicas">
+					<?php
+					//exibindo os valores da tabela do bd
 					$receber = $us->pegar("atividades_academicas");
-					$i=0;
-					while($receber[0][$i]>$i){
+					$i = 0;
+					while ($receber[0][$i] > $i) {
 						$valor = $receber[0][$i];
 						$str = $receber[1][$i];
 						echo "<option value='$valor'>$str</option>";
 						$i++;
 					}
-				?>
-			</select>
-		</div>
-		<div>
-			<p>Como costuma a buscar imagens para elaborar suas atividades acadêmicas?</p>
-			<select name="atividades_imagem">
-				<?php
-				//exibindo os valores da tabela do bd
+					?>
+				</select>
+			</div>
+			<div>
+				<p>Como costuma a buscar imagens para elaborar suas atividades acadêmicas?</p>
+				<select name="atividades_imagem">
+					<?php
+					//exibindo os valores da tabela do bd
 					$receber = $us->pegar("atividades_imagens");
-					$i=0;
-					while($receber[0][$i]>$i){
+					$i = 0;
+					while ($receber[0][$i] > $i) {
 						$valor = $receber[0][$i];
 						$str = $receber[1][$i];
 						echo "<option value='$valor'>$str</option>";
 						$i++;
 					}
-				?>
-			</select>
-		</div>
-		<div>
-			<p>E costuma manter os créditos aos autores das atividades acadêmicas?</p>
-			<span><input type="radio" value="sim" name="credito_autor"> Sim ou
-                  <input type="radio" value="nao" name="credito_autor"> Não</span>
-		</div>
-		<div>
-			<p>O AJUDANDO disponibiliza uma série de atividades elaborados por professores e estudantes de educação.  O que acha disto?</p>
-			<select name="disponibiliza_atividades">
-				<?php
-				//exibindo os valores da tabela do bd
+					?>
+				</select>
+			</div>
+			<div>
+				<p>E costuma manter os créditos aos autores das atividades acadêmicas?</p>
+				<span><input type="radio" value="sim" name="credito_autor"> Sim ou
+					<input type="radio" value="nao" name="credito_autor"> Não</span>
+			</div>
+			<div>
+				<p>O AJUDANDO disponibiliza uma série de atividades elaborados por professores e estudantes de educação. O que acha disto?</p>
+				<select name="disponibiliza_atividades">
+					<?php
+					//exibindo os valores da tabela do bd
 					$receber = $us->pegar("disponibiliza_atividades");
-					$i=0;
-					while($receber[0][$i]>$i){
+					$i = 0;
+					while ($receber[0][$i] > $i) {
 						$valor = $receber[0][$i];
 						$str = $receber[1][$i];
 						echo "<option value='$valor'>$str</option>";
 						$i++;
 					}
-				?>
-			</select>
-		</div>
-		<div>
-			<p>Ao solicitar atividades acadêmicas o AJUDANDO irá envia-las para você por e-mail. Estas atividades podem ser enviadas em 3 formatos. Qual prefere?</p>
+					?>
+				</select>
+			</div>
+			<div>
+				<p>Ao solicitar atividades acadêmicas o AJUDANDO irá envia-las para você por e-mail. Estas atividades podem ser enviadas em 3 formatos. Qual prefere?</p>
 				<span>
-				<?php
-				//exibindo os valores da tabela do bd
+					<?php
+					//exibindo os valores da tabela do bd
 					$receber = $us->pegar("formatos");
-					$i=0;
-					while($receber[0][$i]>$i){
+					$i = 0;
+					while ($receber[0][$i] > $i) {
 						$valor = $receber[0][$i];
 						$str = $receber[1][$i];
 						echo "<input type='radio' value='$valor' name='formato'>$str";
 						$i++;
 					}
-				?>
+					?>
 				</span>
-		</div>
-		</br><input type="submit" value="salvar">
-	</form>
+			</div>
+			</br><input type="submit" value="salvar">
+		</form>
+	</div>
 	<?php
 
 	session_start();
-	
-		if(isset($_POST['formato'])){
-			//pegando as informações
-			$informatica = (int) addslashes($_POST['informatica']);
-			$editor_texto =(int) addslashes($_POST['editores_texto']);
-			$apresentacao =(int) addslashes($_POST['apresentacao']);
-			$imagem_video =(int) addslashes($_POST['imagem_video']);
-			$multimidia =(int) addslashes($_POST['multimidia']);
-			$atividades_academicas =(int) addslashes($_POST['atividades_academicas']);
-			$atividades_imagem =(int) addslashes($_POST['atividades_imagem']);
-			$credito_autor = addslashes($_POST['credito_autor']);
-			$disponibiliza_atividades =(int) addslashes($_POST['disponibiliza_atividades']);
-			$formato =(int) addslashes($_POST['formato']);
-			
-			//salvando as informações
-			$verifica = $us->finalizar($credito_autor, $formato, $informatica, $editor_texto, $apresentacao, $atividades_imagem, $imagem_video, $multimidia, $disponibiliza_atividades, $atividades_academicas);
-			if($verifica == true){
-				//logando 
-				$emial = $_SESSION['salvar_depois'];
-				$senha = $_SESSION['sen'];
-				$verificar = $us->logar($email, $senha);
-				if($verificar == true){
-					header("location: areaprivada.php");
-				}
-				else{
-					header("location: index.php");
-				}
+
+	if (isset($_POST['formato'])) {
+		//pegando as informações
+		$informatica = (int)addslashes($_POST['informatica']);
+		$editor_texto = (int)addslashes($_POST['editores_texto']);
+		$apresentacao = (int)addslashes($_POST['apresentacao']);
+		$imagem_video = (int)addslashes($_POST['imagem_video']);
+		$multimidia = (int)addslashes($_POST['multimidia']);
+		$atividades_academicas = (int)addslashes($_POST['atividades_academicas']);
+		$atividades_imagem = (int)addslashes($_POST['atividades_imagem']);
+		$credito_autor = addslashes($_POST['credito_autor']);
+		$disponibiliza_atividades = (int)addslashes($_POST['disponibiliza_atividades']);
+		$formato = (int)addslashes($_POST['formato']);
+
+		//salvando as informações
+		$verifica = $us->finalizar($credito_autor, $formato, $informatica, $editor_texto, $apresentacao, $atividades_imagem, $imagem_video, $multimidia, $disponibiliza_atividades, $atividades_academicas);
+		if ($verifica == true) {
+			//logando 
+			$emial = $_SESSION['salvar_depois'];
+			$senha = $_SESSION['sen'];
+			$verificar = $us->logar($email, $senha);
+			if ($verificar == true) {
+				header("location: areaprivada.php");
+			} else {
+				header("location: index.php");
 			}
 		}
+	}
 	?>
 </body>
+
 </html>
